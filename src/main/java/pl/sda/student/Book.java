ba89df843f054f0e12d51fcc66d04ecc219b018d
popielozko <maciejd69@gmail.com>
2020-03-01 13:15:11 +0100

    to so moje nowe zmiany

--- src/main/java/pl/sda/student/Book.java
+++ src/main/java/pl/sda/student/Book.java
@@ -1,4 +1,13 @@
 package pl.sda.student;
 
 public class Book {
+    private String name;
+    private Author author;
+    private Category category;
+
+    public Book(String name, Author author, Category category) {
+        this.name = name;
+        this.author = author;
+        this.category = category;
+    }
 }
--- src/main/java/pl/sda/student/Main.java
+++ src/main/java/pl/sda/student/Main.java
@@ -1,8 +1,38 @@
 package pl.sda.student;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Category poezja = new Category("poezja");
+        Author Byron = new Author("Byron", "Kowalski");
+        Book Wiersze = new Book("Wiersze", Byron, poezja);
+
+        Category historia = new Category("historia");
+        Author Artur = new Author("Artur", "Jackowski");
+        Book Dzieje = new Book("Dzieje", Artur, historia);
+        Book Dawno = new Book("Dawno", Artur, historia);
+
+        Category beletrystyka = new Category("beletrystyka");
+        Author Baba = new Author("Baba", "Gupia");
+        Book Beksa = new Book("Beksa", Baba, historia);
+
+        List<Book> Lista = new ArrayList<>();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+
+            objectMapper.writeValue(new File("Lista.json"), Lista);
+        }
+        catch (IOException e)   {
+            e.printStackTrace();
+        }
     }
 }
